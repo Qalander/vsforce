cc299e4ac7450bed82012f21e06f64bfc4d2b6c7
Etienne Rocheleau <erocheleau@coveo.com>
2016-04-29 14:46:59 -0400

    Deploy command

--- src/extension.ts
+++ src/extension.ts
@@ -4,15 +4,17 @@ import * as vscode from 'vscode';
 import {Connection} from './connection'
 
 let jsforce = require('jsforce');
+let fs = require('fs');
+var zipFolder = require('zip-folder');
 
 export function activate(context: vscode.ExtensionContext) {
     var conn = new Connection();
 
     let executeQuery = vscode.commands.registerCommand('extension.executeQuery', () => {
-        vscode.window.showInputBox({prompt: "Query: "}).then(query => conn.executeQuery(query, (res: any) => {console.log(res)}));
+        vscode.window.showInputBox({ prompt: "Query: " }).then(query => conn.executeQuery(query, (res: any) => { console.log(res) }));
     })
-    context.subscriptions.push(executeQuery); 
-    
+    context.subscriptions.push(executeQuery);
+
     let executeCode = vscode.commands.registerCommand('extension.executeCode', () => {
         var editor = vscode.window.activeTextEditor;
         if (!editor) {
@@ -22,6 +24,29 @@ export function activate(context: vscode.ExtensionContext) {
         conn.executeCode(query);
     });
     context.subscriptions.push(executeCode);
+
+    let sfdeploy = vscode.commands.registerCommand('extension.sfdeploy', () => {
+        vscode.workspace.findFiles("package.xml", "").then((value: vscode.Uri[]) => {
+            // Zip current folder
+            zipFolder(vscode.workspace.rootPath, '/toDeploy.zip', (err: any) => {
+                if(err) {return console.error(err)}
+                var zipStream = fs.createReadStream("toDeploy.zip");
+                conn.execute((conn: any) => {
+                    conn.metadata.deploy(zipStream, {"allowMissingFiles": true, "autoUpdatePackage": true, "singlePackage": true})
+                    .complete(function(err, result) {
+                        if (err) {return console.error(err);}
+                        console.log('done ? :' + result.done);
+                        console.log('success ? : ' + result.true);
+                        console.log(result)
+                    })
+                })
+            })
+            // deploy
+        },
+            (reason: any) => {
+                console.error(reason);
+            });
+    });
 }
 
 // this method is called when your extension is deactivated
